fb9d996467b2f72606019df4f208d3007428d8d7
Hussein Al Hadad <Hussein1985@hotmail.com>
2020-06-21 16:38:03 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        // Represents the table name in database
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
