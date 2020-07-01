48e7f7242d03bd16c0d83197ab1ef955ab52cc7f
Hussein Al Hadad <Hussein1985@hotmail.com>
2020-07-01 22:22:20 +0200

    Add navbar and login

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
+  declarations: [AppComponent, ValueComponent, NavComponent],
   imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
