889e633ba79c7479fd51f79755e1e75e2b49d72b
Hussein Al Hadad <Hussein1985@hotmail.com>
2020-07-05 10:15:57 +0200

    Make token persisted after pagerefresh

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -9,6 +9,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) { }
 
@@ -17,7 +18,11 @@ export class AuthService {
       map((response: any) => {
         const user = response;
         if (user) {
+
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+
+          console.log(this.decodedToken);
         }
       })
     );
