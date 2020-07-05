6836e3c5d49f94b82513d1e02a31bf782ace30ed
Hussein Al Hadad <Hussein1985@hotmail.com>
2020-07-05 08:42:28 +0200

    Add alertifyjs for messages

--- DatingApp-SPA/src/app/_services/error.intercepter.ts
+++ DatingApp-SPA/src/app/_services/error.intercepter.ts
@@ -6,41 +6,41 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		return next.handle(req).pipe(
-			catchError(error => {
-				if (error.status === 401) {
-					return throwError(error.statusText);
-				}
-				if (error instanceof HttpErrorResponse) {
-					const applicationError = error.headers.get('Application-Error');
-					if (applicationError) {
-						return throwError(applicationError);
-					}
-					const serverError = error.error;
-					let modalStateError = '';
-					if (serverError.error && typeof serverError.errors === 'object') {
-						for (const key in serverError.error) {
-							if (serverError.errors[key]) {
-								modalStateError += serverError.errors[key] + '\n';
-							}
-						}
-					}
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError(error => {
+                if (error.status === 401) {
+                    return throwError(error.statusText);
+                }
+                if (error instanceof HttpErrorResponse) {
+                    const applicationError = error.headers.get('Application-Error');
+                    if (applicationError) {
+                        return throwError(applicationError);
+                    }
+                    const serverError = error.error;
+                    let modalStateErrors = '';
+                    if (serverError.errors && typeof serverError.errors === 'object') {
+                        for (const key in serverError.errors) {
+                            if (serverError.errors[key]) {
+                                modalStateErrors += serverError.errors[key] + '\n';
+                            }
+                        }
+                    }
 
-					return throwError(modalStateError || serverError || 'Server Error');
+                    return throwError(modalStateErrors || serverError || 'Server Error');
 
-				}
+                }
 
-			})
-		);
+            })
+        );
 
 
-	}
+    }
 
 }
 
 export const ErrorInterceptorProvider = {
-	provide: HTTP_INTERCEPTORS,
-	useClass: ErrorInterceptor,
-	multi: true
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorInterceptor,
+    multi: true
 };
