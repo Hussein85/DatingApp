ac2554c5f3beb9b1ba6c7d071845d6facf2dfd7d
Hussein Al Hadad <Hussein1985@hotmail.com>
2020-07-08 20:22:05 +0200

    Add autoMapper

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -8,34 +8,35 @@ using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = CreateHostBuilder(args).Build();
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var Services = scope.ServiceProvider;
-                try
-                {
-                    var context = Services.GetRequiredService<DataContext>();
-                    context.Database.Migrate();
-                    Seed.SeedUsers(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = Services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occured during migration");
-                }
-            }
-        }
+			using (var scope = host.Services.CreateScope())
+			{
+				var Services = scope.ServiceProvider;
+				try
+				{
+					var context = Services.GetRequiredService<DataContext>();
+					context.Database.Migrate();
+					Seed.SeedUsers(context);
+				}
+				catch (Exception ex)
+				{
+					var logger = Services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An error occured during migration");
+				}
+			}
+			host.Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }
