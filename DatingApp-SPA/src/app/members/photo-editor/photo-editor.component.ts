0dedad438ba85980c7465bc8d2804ccb3a449bb5
Hussein Al Hadad <Hussein1985@hotmail.com>
2020-07-14 00:12:00 +0200

    Update mainphoto in SPA after changing picture

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Photo } from 'src/app/_models/photo';
 import { FileUploader } from 'ng2-file-upload';
 import { environment } from 'src/environments/environment';
@@ -13,10 +13,12 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 })
 export class PhotoEditorComponent implements OnInit {
     @Input() photos: Photo[];
+    @Output() getMemberPhotoChange = new EventEmitter<string>();
 
     uploader: FileUploader;
     hasBaseDropZoneOver: boolean;
     baseUrl = environment.apiUrl;
+    currentMain: Photo;
 
     response: string;
 
@@ -59,10 +61,13 @@ export class PhotoEditorComponent implements OnInit {
 
     setMainPhoto(photo: Photo) {
         this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
-            console.log('Successfully set to main');
+            this.currentMain = this.photos.filter(p => p.isMain === true)[0];
+            this.currentMain.isMain = false;
+            photo.isMain = true;
+            this.getMemberPhotoChange.emit(photo.url);
         }, error => {
             this.alertify.error(error);
-        })
+        });
     }
 
 }
