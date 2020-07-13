6eb4178bfcb87dc061516b69f470ffd4fe881e3e
Hussein Al Hadad <Hussein1985@hotmail.com>
2020-07-14 01:05:49 +0200

    Add the main photo to the Nav bar

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -4,37 +4,41 @@ import { AlertifyService } from '../_services/alertify.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css'],
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  model: any = {};
+    model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
+    constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
-  ngOnInit(): void { }
+    ngOnInit(): void { }
 
-  login() {
-    this.authService.login(this.model).subscribe(
-      (next) => {
-        this.alertify.success('Logged in successfully');
-      },
-      (error) => {
-        this.alertify.error(error);
-      }, () => {
-        this.router.navigate(['/members']);
-      }
-    );
-  }
+    login() {
+        this.authService.login(this.model).subscribe(
+            (next) => {
+                this.alertify.success('Logged in successfully');
+            },
+            (error) => {
+                this.alertify.error(error);
+            }, () => {
+                this.router.navigate(['/members']);
+            }
+        );
+    }
 
-  loggedIn() {
-    return this.authService.loggedIn();
-  }
+    loggedIn() {
+        return this.authService.loggedIn();
+    }
 
-  logout() {
-    localStorage.removeItem('token');
-    this.alertify.message('Logged out');
-    this.router.navigate(['/home']);
-  }
+    logout() {
+
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        this.authService.decodedToken = null;
+        this.authService.currentUser = null;
+        this.alertify.message('Logged out');
+        this.router.navigate(['/home']);
+    }
 }
